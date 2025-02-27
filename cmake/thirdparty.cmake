ba6e846085747564fcd034b490f464aa0b4282d0
Thiago Cavalcante <159804604+jssfr@users.noreply.github.com>
2025-02-26 22:28:57 -0300

    fix cmakelists

--- cmake/thirdparty.cmake
+++ cmake/thirdparty.cmake
@@ -1,11 +1,11 @@
 # This commands downloads AND configures fmt. It sets up some variables.
 CPMAddPackage(
     NAME fmt
-    GIT_TAG 11.1.1
-    VERSION 11.1.1
+    GIT_TAG 11.1.3
+    VERSION 11.1.3
     GITHUB_REPOSITORY fmtlib/fmt
     SOURCE_DIR ${THIRDY_DIR}/fmt
 )
-#CPMAddPackage("gh:fmtlib/fmt#11.1.1") # other way of use
+#CPMAddPackage("gh:fmtlib/fmt#11.1.3") # other way of use
 
 
